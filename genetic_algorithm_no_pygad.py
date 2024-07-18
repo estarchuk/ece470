4ea78007a9552ec781de07a6baba8dfb37073efe
estarchuk <estarchuk@uvic.ca>
2024-07-18 13:28:43 -0700

    Code cleanup, more comments

--- genetic_algorithm_no_pygad.py
+++ genetic_algorithm_no_pygad.py
@@ -5,37 +5,39 @@ import pandas as pd
 import random
 from datetime import datetime
 
-function_inputs = [[1,2,3,4,5,6,7,8,9], [2,3,4,5,1,25,36,78,98], [3,4,5,1,2], [4,5,1,2,3]]
-random_test = []
+gene_sets = [[1,2,3,4,5,6,7,8,9], [2,3,4,5,1,25,36,78,98], [3,4,5,1,2,56,84,12,35], [4,5,1,2,3,21,35,83,67]]
+crossover_mutation_test_genes = []
 length  = 9
 
-def crossover(function_input1, function_input2):
-    global random_test
+# Crossover function, picks a semi-random amount of genes per parent to give to offspring
+def crossover(parent1, parent2):
+    global crossover_mutation_test_genes
     random.seed()
     x_alleles  = random.randrange(1,length)
     y_alleles = length - x_alleles
     for i in range(x_alleles):
-        random_test.append(function_input1[i])
+        crossover_mutation_test_genes.append(parent1[i])
     for i in range(y_alleles):
-        random_test.append(function_input2[x_alleles + i])
+        crossover_mutation_test_genes.append(parent2[x_alleles + i])
 
+# Mutation function, picks a random gene(s) from a given set to mutate
 def mutation():
-    global random_test
+    global crossover_mutation_test_genes
     random.seed()
     chance = random.randrange(0,100)
     mutation_alleles = []
     if(chance <= 10):
-        # Depending on the allele selected we'll need acceptable ranges
+        # Depending on the gene(s) selected we'll need acceptable ranges, 0-100 for now
         mutation_amount = random.randrange(1,3)
         for i in range(mutation_amount):
-            mutation_alleles.append(random.randrange(0, len(random_test)))
+            mutation_alleles.append(random.randrange(0, len(crossover_mutation_test_genes)))
         for alleles in mutation_alleles:
-            random_test[alleles] += 100000
+            crossover_mutation_test_genes[alleles] += random.randrange(1,101)
 
 def main():
-    crossover(function_inputs[0], function_inputs[1])
+    crossover(gene_sets[0], gene_sets[1])
     mutation()
-    print(random_test)
+    print(crossover_mutation_test_genes)
 
 
 if __name__ == "__main__":
